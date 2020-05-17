12cca604b14cfd70840c6d3e0b326443c47312d4
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2020-05-17 21:48:56 +0300

    Сделал рабочий функционал JS,добавил страницу с номером,добавил коментарии

--- script/script.js
+++ script/script.js
@@ -1,134 +1,190 @@
-document.addEventListener('DOMContentLoaded', function() {
-
+// обработчик событий, который отслеживает закгузку контента
+document.addEventListener("DOMContentLoaded", function() {
     const btnOpenModal = document.querySelector("#btnOpenModal");
     const modalBlock = document.querySelector("#modalBlock");
     const closeModal = document.querySelector("#closeModal");
     const questionTitle = document.querySelector("#question");
-    const formAnswers = document.querySelector('#formAnswers');
-    const prevButton = document.querySelector('#prev');
-    const nextButton = document.querySelector('#next');
+    const formAnswers = document.querySelector("#formAnswers");
+    const prevButton = document.querySelector("#prev");
+    const nextButton = document.querySelector("#next");
+    const sendButton = document.querySelector("#send");
 
+    // обьект содержащий вопросы и ответы
     const questions = [{
             question: "Какого цвета бургер?",
             answers: [{
-                    title: 'Стандарт',
-                    url: './image/burger.png'
+                    title: "Стандарт",
+                    url: "./image/burger.png",
                 },
                 {
-                    title: 'Черный',
-                    url: './image/burgerBlack.png'
-                }
+                    title: "Черный",
+                    url: "./image/burgerBlack.png",
+                },
             ],
-            type: 'radio'
+            type: "radio",
         },
         {
             question: "Из какого мяса котлета?",
             answers: [{
-                    title: 'Курица',
-                    url: './image/chickenMeat.png'
+                    title: "Курица",
+                    url: "./image/chickenMeat.png",
                 },
                 {
-                    title: 'Говядина',
-                    url: './image/beefMeat.png'
+                    title: "Говядина",
+                    url: "./image/beefMeat.png",
                 },
                 {
-                    title: 'Свинина',
-                    url: './image/porkMeat.png'
-                }
+                    title: "Свинина",
+                    url: "./image/porkMeat.png",
+                },
             ],
-            type: 'radio'
+            type: "radio",
         },
         {
             question: "Дополнительные ингредиенты?",
             answers: [{
-                    title: 'Помидор',
-                    url: './image/tomato.png'
+                    title: "Помидор",
+                    url: "./image/tomato.png",
                 },
                 {
-                    title: 'Огурец',
-                    url: './image/cucumber.png'
+                    title: "Огурец",
+                    url: "./image/cucumber.png",
                 },
                 {
-                    title: 'Салат',
-                    url: './image/salad.png'
+                    title: "Салат",
+                    url: "./image/salad.png",
                 },
                 {
-                    title: 'Лук',
-                    url: './image/onion.png'
-                }
+                    title: "Лук",
+                    url: "./image/onion.png",
+                },
             ],
-            type: 'checkbox'
+            type: "checkbox",
         },
         {
             question: "Добавить соус?",
             answers: [{
-                    title: 'Чесночный',
-                    url: './image/sauce1.png'
+                    title: "Чесночный",
+                    url: "./image/sauce1.png",
                 },
                 {
-                    title: 'Томатный',
-                    url: './image/sauce2.png'
+                    title: "Томатный",
+                    url: "./image/sauce2.png",
                 },
                 {
-                    title: 'Горчичный',
-                    url: './image/sauce3.png'
-                }
+                    title: "Горчичный",
+                    url: "./image/sauce3.png",
+                },
             ],
-            type: 'radio'
-        }
+            type: "radio",
+        },
     ];
 
     // открывает модальное окно
-    btnOpenModal.addEventListener('click', () => {
-        modalBlock.classList.add('d-block');
+    btnOpenModal.addEventListener("click", () => {
+        modalBlock.classList.add("d-block");
         playTest();
     });
 
     // закрывает модальное окно
-    closeModal.addEventListener('click', () => {
-        modalBlock.classList.remove('d-block');
+    closeModal.addEventListener("click", () => {
+        modalBlock.classList.remove("d-block");
     });
 
+    // функция запуска тестирования
     const playTest = () => {
+        const finalAnswers = [];
+        //переменная с номером вопроса
         let numberQuestion = 0;
-
+        // функция перебирает масив з ответами, и динамичиски выводит их на страницу
         const renderAnswers = (index) => {
             questions[index].answers.forEach((answer, index, arr) => {
-                const answerItem = document.createElement('div');
+                const answerItem = document.createElement("div");
 
-                answerItem.classList.add('answers-item', 'd-flex', 'flex-column');
+                answerItem.classList.add("answers-item", "d-flex", "justify-content-center");
 
-                answerItem.innerHTML =
-                    `
-                    <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none">
+                answerItem.innerHTML = `
+                    <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none" value="${answer.title}">
                     <label for="${answer.title}" class="d-flex flex-column justify-content-between">
                         <img class="answerImg" src="${answer.url}" alt="burger">
                         <span>${answer.title}</span>
                     </label>
-                    `
+                    `;
                 formAnswers.appendChild(answerItem);
-            })
-        }
-
+            });
+        };
+        // функция рендинга вопросов + ответов
         const renderQuestions = (indexQuestion) => {
-            formAnswers.innerHTML = '';
-            questionTitle.textContent = `${questions[indexQuestion].question}`;
+            formAnswers.innerHTML = "";
+            if (numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
+                questionTitle.textContent = `${questions[indexQuestion].question}`;
+                renderAnswers(indexQuestion);
+                nextButton.classList.remove("d-none");
+                prevButton.classList.remove("d-none");
+                sendButton.classList.add("d-none");
+            }
 
-            renderAnswers(indexQuestion);
-        }
+            if (numberQuestion === 0) {
+                prevButton.classList.add("d-none");
+            }
 
+            if (numberQuestion === questions.length) {
+                nextButton.classList.add("d-none");
+                prevButton.classList.add("d-none");
+                sendButton.classList.remove("d-none");
+                formAnswers.innerHTML = `
+                <div class="form-group">
+                    <label for="numberPhone">Введити Ваш номер</label> 
+                    <input type="phone" class="form-control" id="numberPhone">
+                </div>`;
+            }
+
+            if (numberQuestion === questions.length + 1) {
+                formAnswers.textContent = 'Спасибо за пройдений тест';
+                setTimeout(() => {
+                    modalBlock.classList.remove('d-block')
+                }, 2000);
+            }
+        };
+        // запуск функции рендинга
         renderQuestions(numberQuestion);
 
-        // реализация кнопок вперед и назад
+        const checkAnswer = () => {
+            const obj = {};
+
+            const inputs = [...formAnswers.elements].filter((input) => input.checked || input.id === 'numberPhone');
+
+            inputs.forEach((input, index) => {
+                if (numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
+                    obj[`${index}_${questions[numberQuestion].question}`] = input.value;
+                }
+
+                if (numberQuestion === questions.length) {
+                    obj['Номер телефона'] = input.value;
+                }
+            });
+
+            finalAnswers.push(obj)
+
+        };
+
+        // обработчики событий кнопок next и prev
         nextButton.onclick = () => {
+            checkAnswer();
             numberQuestion++;
             renderQuestions(numberQuestion);
-        }
+        };
 
         prevButton.onclick = () => {
             numberQuestion--;
             renderQuestions(numberQuestion);
-        }
-    }
+        };
 
+        sendButton.onclick = () => {
+            checkAnswer();
+            numberQuestion++;
+            renderQuestions(numberQuestion);
+            console.log(finalAnswers);
+        }
+    };
 });
